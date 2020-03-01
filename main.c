227a5b160a348f53361002ed4c08bd525a1ff10f
Caio César <br.caiorecife@cesarleitao.com.br>
2020-02-29 22:30:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> //add library "stdio.h"
 
-main(){
-int num = 0;
-  
-  printf("Insira um numero: ");
-  scanf(" %i ", &num);
-    
-    printf("\n O numero escolhido foi: %i", num);
+main ()   //main class
+{
+    int num = 0;   //int say integer variable.num is variable name.= 0 is good practice
+
+    printf ("Insira um numero: ");   //print("") allow write txt
+    scanf ("%i", &num);
+    /*scan("") receive chose number.
+    ("%i" indicate int-type variable'll appear, in printf, indicate chose number'll be int
+    , &num) indicate chose number is atribued to num*/
+
+    printf ("\nO numero escolhido foi: %i", num);
+    //("\n is line break. ". num) say %i is num 
 }
+
+/*extra notes!
+variables can be:
+float(decimal, limited);
+double(decimal, infinite);
+char(character);
+boolean(true or false)
+
+f = formation
